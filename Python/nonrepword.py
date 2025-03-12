8a02c8c98b6eb053aaea0818b759248974b29583
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-03-12 02:33:36 +0000

    comments added for non repeating

--- Python/nonrepword.py
+++ Python/nonrepword.py
@@ -1,3 +1,19 @@
+'''
+To Find the non-repeated word in a given string
+
+Assign the required string to the variable by making it as lower case and removing the space, and 
+initialize the empty dictionary which going to hold each word as key and number of occurrences as value.
+
+Iterate over each word in the sentence and if the word is not in the dict then add it by assigning the
+value as 1, if the word already exits then increment the value by 1
+
+now the dictionary holds the words and no of occurrence.
+
+now iterate over the dictionary and if the value is 1 then stop  as it is the first non-repeating 
+word in the sentence
+
+'''
+
 sent = "A guitar has six strings".lower().replace(" ","")
 
 nonrep = dict()
