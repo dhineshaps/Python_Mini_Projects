7567706c7dccb9933e0a17e5eb84c0566e41b819
Dhinesh Palanisamy <84257275+dhineshaps@users.noreply.github.com>
2025-03-12 02:42:27 +0000

    palindrome comments added

--- Python/palindrome.py
+++ Python/palindrome.py
@@ -1,3 +1,14 @@
+'''
+To Find the given word is Palindrome.
+
+Made the given word as list variable and initialized a empty string variable.
+
+Iterated the given word list with range, so that no of times the negative index will 
+have the value from the last and it is stored in the initialized empty variable.
+
+finally the  given word and string variable is compared, if both are same then it is palindrome
+'''
+
 word="malayalam"
 lword = list(word)
 palword=""
